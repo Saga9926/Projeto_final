101ebd5bd01cb726271fbd4da05ddd5a88503965
Saga9926 <cupelliscjoaopedro@gmail.com>
2025-12-04 22:05:03 +0000

    Add files via upload

---
+++ bibliotecas/machine-0.0.1/#Lista de variaveis utilizada no(s) prog.py
@@ -0,0 +1,7 @@
+#Lista de variaveis utilizada no(s) programa(s):
+#import = importa bibliotecas externas
+#for _ in _ = repetição com variaveis estipuladas
+#def = define uma função
+#set_angle = define o angulo de um servo motor
+#while = repetição com condição estipulada
+
